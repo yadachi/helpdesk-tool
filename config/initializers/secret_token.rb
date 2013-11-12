370a4c9101c2b4930dec33dbbc5905e17a54ca0a
Yosuke Adachi <front.lightstone@gmail.com>
2013-11-12 19:51:24 +0000

    rails new helpdesk -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpdesk::Application.config.secret_key_base = '71c384a0ce570172f6ed46a9b5c791757f67cf49418655e42c3e4efbf49546a26c635bc82d1a3afb694be12bd1993a9c9e6413dac7e7c6a3c3c868c03f889e57'
