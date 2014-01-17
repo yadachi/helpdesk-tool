64be28dc8b2ce68ebea46f6576e0c9325056a84f
Yosuke Adachi <front.lightstone@gmail.com>
2014-01-17 17:32:10 +0000

    fixed syntax error

--- db/migrate/20140117124905_create_customers.rb
+++ db/migrate/20140117124905_create_customers.rb
@@ -1,10 +1,10 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :title, :limit => 10,
-      t.string :name, :limit => 60,
-      t.string :direct_number, :limit => 20,
-      t.string :email, :limit => 60,
+      t.string :title, :limit => 10
+      t.string :name, :limit => 60
+      t.string :direct_number, :limit => 20
+      t.string :email, :limit => 60
 
       t.timestamps
     end
