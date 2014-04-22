f15cb43961a02eab02c85db1e741751981798f48
root <root@scsedev1.(none)>
2014-04-22 13:52:25 +0100

    Seed file modification.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Test Records for Issue
+Issue.transaction do
+	(1..10).each do |i|
+		Issue.create(:company_name => i % 3, :summary => "Issue Summary #{i}", :received_date => Date.today, :due_date => Date.today + 7, :status => i % 3, :issue_type => i % 3, :assigned_to => i % 3)
+	end
+end
